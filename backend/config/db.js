43b1b0c7885005c3189219e0a515b976812f50b6
Thiago Albertino <thiago.albertino2@justeattakeaway.com>
2026-01-17 15:52:39 -0700

    database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/english_poc');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/english_poc', {
+      dbName: process.env.MONGO_DB_NAME
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
